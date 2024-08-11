c37599099e447fda1163091a634d37c721f35eea
Vedran Rodic <vrodic@gmail.com>
2024-08-11 19:31:35 +0200

    default route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'planner/index'
   get 'planner/show'
   get 'planner/delays'
+  get '/' => 'planner#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
