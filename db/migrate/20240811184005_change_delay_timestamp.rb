6ecc47758c12ec474587a55a5835b15ef54bfe35
Vedran Rodic <vrodic@gmail.com>
2024-08-12 11:17:01 +0200

    slience bots

--- db/migrate/20240811184005_change_delay_timestamp.rb
+++ db/migrate/20240811184005_change_delay_timestamp.rb
@@ -1,7 +1,7 @@
 class ChangeDelayTimestamp < ActiveRecord::Migration[7.2]
   def up
     count = 0
-    RideDelayLog.where("timestamp LIKE('%sati')").each do |delay|
+    RideDelayLog.where("timestamp LIKE('%sati')").find_each do |delay|
       next unless delay.timestamp
 
       count += 1
@@ -10,10 +10,10 @@ class ChangeDelayTimestamp < ActiveRecord::Migration[7.2]
       begin
         delay.update(timestamp: Time.zone.strptime(timestamp, '%d.%m.%y. %H:%M'))
       rescue Date::Error
-        puts "TS: #{timestamp}"
+        Rails.logger.debug "TS: #{timestamp}"
       end
     end
-    puts count
+    Rails.logger.debug count
 
     change_column :ride_delay_logs, :timestamp, :datetime
   end
